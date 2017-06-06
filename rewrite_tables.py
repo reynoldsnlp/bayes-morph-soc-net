8241b20067c97cff95049b8a50de6a0da80ef040
nlpcall <beboppinbobby@gmail.com>
2017-06-06 12:25:25 -0600

    function-ized write_tables().

--- rewrite_tables.py
+++ rewrite_tables.py
@@ -29,37 +29,42 @@ def get_table_dict(agent):
     return table_dict
 
 
-filename = sys.argv[1]
-if '/' in filename:
-    filename = filename.split('/')[1]
-print('Input file is {}.'.format(filename))
-filename = filename.rstrip('.pickle')
-print('Output filename is {}.'.format(filename))
+def write_tables(model, filename):
+    """Write morphological tables to a file with same name as pickled file."""
+    with open('results/' + filename + '_TABLES.txt', 'w') as table_file:
+        gen_tracker = None
+        for a in model.schedule.agents:
+            if a.gen_id != gen_tracker:
+                gen_tracker = a.gen_id
+                print('=' * 79, file=table_file)
+                print('=' * 79, file=table_file)
+                print('Generation {}'.format(gen_tracker), file=table_file)
+                print('=' * 79, file=table_file)
+                print('=' * 79, file=table_file)
+            table_dict = get_table_dict(a)
+            print('=' * 39, file=table_file)
+            print('Gen: {},    Agent: {}'.format(a.gen_id, a.unique_id),
+                  file=table_file)
+            if isinstance(table_dict, str):
+                print(table_dict, file=table_file)
+            else:
+                header = '\t'.join([''] + [m for m in a.model.seed_MSPSs])
+                print(header, file=table_file)
+                for e_list, type_freq in sorted(table_dict.items(),
+                                                key=lambda x: x[1],
+                                                reverse=True):
+                    print(type_freq, *e_list, sep='\t', file=table_file)
 
-print('Loading pickled model into memory... ({})'.format(filename))
-with open('results/' + filename + '.pickle', 'rb') as p_file:
-    model = pickle.load(p_file)
 
-with open('results/' + filename + '_TABLES.txt', 'w') as table_file:
-    gen_tracker = None
-    for a in model.schedule.agents:
-        if a.gen_id != gen_tracker:
-            gen_tracker = a.gen_id
-            print('=' * 79, file=table_file)
-            print('=' * 79, file=table_file)
-            print('Generation {}'.format(gen_tracker), file=table_file)
-            print('=' * 79, file=table_file)
-            print('=' * 79, file=table_file)
-        table_dict = get_table_dict(a)
-        print('=' * 39, file=table_file)
-        print('Gen: {},    Agent: {}'.format(a.gen_id, a.unique_id),
-              file=table_file)
-        if isinstance(table_dict, str):
-            print(table_dict, file=table_file)
-        else:
-            header = '\t'.join([''] + [m for m in a.model.seed_MSPSs])
-            print(header, file=table_file)
-            for e_list, type_freq in sorted(table_dict.items(),
-                                            key=lambda x: x[1],
-                                            reverse=True):
-                print(type_freq, *e_list, sep='\t', file=table_file)
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    if '/' in filename:
+        filename = filename.split('/')[1]
+    print('Input file is {}.'.format(filename))
+    filename = filename.rstrip('.pickle')
+    print('Output filename is {}.'.format(filename))
+
+    print('Loading pickled model into memory... ({})'.format(filename))
+    with open('results/' + filename + '.pickle', 'rb') as p_file:
+        model = pickle.load(p_file)
+    write_tables(model, filename)
