2428a93b93e7d2f07f7d320757ae713af14bf2f1
nlpcall <beboppinbobby@gmail.com>
2017-06-20 08:25:31 -0600

    adding input tables for the last batch

--- results/100000_rand_4/extract_input_table.sh
+++ results/100000_rand_4/extract_input_table.sh
@@ -3,11 +3,13 @@
 # This script takes one argument, the numeric part of data_x.y.txt
 # Ex: $ sh extract_input_table.sh 4.14
 
-pattern="*$1.log"
-for each in $(ls ${pattern})
+for input in 4.{1..14}
 do
-	table=$(echo $each | sed 's/\.log/_input_table.txt/g')
-	echo "generating ${table} ..."
-	paste ../../artificial-data/id_column_24.tmp <(paste <(printf "typeFreq,$(grep lexeme_type_freq_list ${each} | cut -d [ -f 2 | sed 's/]//g' | sed 's/ //g')" | tr , \\n) "../../artificial-data/data_$1.txt" | cut -f 1,3-) > ${table}
+	pattern="*${input}.log"
+	for each in $(ls ${pattern})
+	do
+		table=$(echo $each | sed 's/\.log/_input_table.txt/g')
+		echo "generating ${table} ..."
+		paste ../../artificial-data/id_column_24.tmp <(paste <(printf "typeFreq,$(grep lexeme_type_freq_list ${each} | cut -d [ -f 2 | sed 's/]//g' | sed 's/ //g')" | tr , \\n) "../../artificial-data/data_${input}.txt" | cut -f 1,3-) > ${table}
+	done
 done
-
