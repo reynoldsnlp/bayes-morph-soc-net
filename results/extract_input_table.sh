ddee25933e8ed50c6609f245bcbf2e4ef4001e96
nlpcall <reynoldsnlp@users.noreply.github.com>
2017-07-08 22:11:43 -0600

    generalized input table extraction script a bit (not completely). adding more 4.x and 5.18 results.

--- results/extract_input_table.sh
+++ results/extract_input_table.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for input in 5.{1..23}
+for input in {4..5}.{1..23}
 do
 	pattern="*${input}.log"
 	for each in $(ls ${pattern})
 	do
 		table=$(echo $each | sed 's/\.log/_input_table.txt/g')
 		echo "generating ${table} ..."
-		paste ../../artificial-data/id_column_24.tmp <(paste <(printf "typeFreq,$(grep lexeme_type_freq_list ${each} | cut -d [ -f 2 | sed 's/]//g' | sed 's/ //g')" | tr , \\n) "../../artificial-data/data_${input}.txt" | cut -f 1,3-) > ${table}
+		paste /Users/robertreynolds/research/bayes-morph-soc-net/artificial-data/id_column_24.tmp <(paste <(printf "typeFreq,$(grep lexeme_type_freq_list ${each} | cut -d [ -f 2 | sed 's/]//g' | sed 's/ //g')" | tr , \\n) "/Users/robertreynolds/research/bayes-morph-soc-net/artificial-data/data_${input}.txt" | cut -f 1,3-) > ${table}
 	done
 done
