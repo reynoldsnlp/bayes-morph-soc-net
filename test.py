3312c3ba2fdfda78eda4d6aea947a87014c3f83f
nlpcall <reynoldsnlp@users.noreply.github.com>
2017-12-30 09:12:25 -0700

    results of flat_tok_freq/no_prior_weight flat_tok_freq/proportional_prior_weight_60k zipf_tok_freq/proportional_prior_weight_60k

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ import bmsn
 import rewrite_tables as rt
 
 
-# data files to focus on: 1, 4, 7, 11
+# TODO(RJR) implement argparse
 OUT_FILENAME_BASE = 'test'
 TIMESTAMP = dt.datetime.now().isoformat().replace(':', '-').split('.')[0]
 GEN_SIZE = 50
@@ -34,6 +34,8 @@ except IndexError:
 CONNECTEDNESS = 0.05
 try:
     PRIOR_WEIGHT = sys.argv[3]
+    if PRIOR_WEIGHT == '':
+        PRIOR_WEIGHT = None
 except IndexError:
     PRIOR_WEIGHT = None
 try:
@@ -73,15 +75,15 @@ def get_data(agent, function, data_key):
 
 def draw_boxplots(model, out_filename=OUT_FILENAME, bootstrapping=False):
     """Draw boxplots of agents' attributes across generations."""
-    print('Running draw_boxplots...', file=sys.stderr)
+    lg.info('Running draw_boxplots...')
     gen_count = model.gen_count
 
-    print('    lex_size...', file=sys.stderr)
+    lg.info('    lex_size...')
     try:
         lex_sizes = [get_data(a, bmsn.lex_size, 'lex_size')
                      for a in model.schedule.agents]
     except (AttributeError, KeyError) as error:
-        print('        ...not in model. Computing from scratch...')
+        lg.info('        ...not in model. Computing from scratch...')
         lex_sizes = [bmsn.lex_size(a) for a in model.schedule.agents]
     ymax = max(lex_sizes)
     if ymax <= 2.0:
@@ -92,12 +94,12 @@ def draw_boxplots(model, out_filename=OUT_FILENAME, bootstrapping=False):
     plt.savefig('results/' + out_filename + '_lex_size.png')
     plt.clf()
 
-    print('    class_count...', file=sys.stderr)
+    lg.info('    class_count...')
     try:
         class_counts = [get_data(a, bmsn.class_counter, 'class_count')
                         for a in model.schedule.agents]
     except (AttributeError, KeyError) as error:
-        print('        ...not in model. Computing from scratch...')
+        lg.info('        ...not in model. Computing from scratch...')
         class_counts = [bmsn.class_counter(a) for a in model.schedule.agents]
     ymax = max(class_counts)
     if ymax <= 2.0:
@@ -108,12 +110,12 @@ def draw_boxplots(model, out_filename=OUT_FILENAME, bootstrapping=False):
     plt.savefig('results/' + out_filename + '_class_count.png')
     plt.clf()
 
-    print('    exp_count...', file=sys.stderr)
+    lg.info('    exp_count...')
     try:
         exp_counts = [get_data(a, bmsn.exp_counter, 'exp_count')
                       for a in model.schedule.agents]
     except (AttributeError, KeyError) as error:
-        print('        ...not in model. Computing from scratch...')
+        lg.info('        ...not in model. Computing from scratch...')
         exp_counts = [bmsn.exp_counter(a) for a in model.schedule.agents]
     ymax = max(exp_counts)
     if ymax <= 2.0:
@@ -124,77 +126,77 @@ def draw_boxplots(model, out_filename=OUT_FILENAME, bootstrapping=False):
     plt.savefig('results/' + out_filename + '_exp_count.png')
     plt.clf()
 
-    print('    decl_entropy...', file=sys.stderr)
+    lg.info('    decl_entropy...')
     try:
         decl_entropies = [get_data(a, bmsn.decl_entropy, 'decl_entropy')
                           for a in model.schedule.agents]
     except (AttributeError, KeyError) as error:
-        print('        ...not in model. Computing from scratch...')
+        lg.info('        ...not in model. Computing from scratch...')
         decl_entropies = [bmsn.decl_entropy(a) for a in model.schedule.agents]
     ymax = max(decl_entropies)
     if ymax <= 2.0:
         plt.ylim((-0.1, 2.1))
     else:
         plt.ylim((-0.1, ymax + 0.1))
-        print('WARN: maximum value over 2.0!', file=sys.stderr)
+        lg.warn('maximum value over 2.0!')
     plt.boxplot(generationalize(decl_entropies, gen_count))
     plt.savefig('results/' + out_filename + '_decl_entropy.png')
     plt.clf()
 
-    print('    avg_cell_entropy...', file=sys.stderr)
+    lg.info('    avg_cell_entropy...')
     try:
         avg_cell_entropies = [get_data(a, bmsn.avg_cell_entropy, 'avg_cell_entropy')  # noqa
                               for a in model.schedule.agents]  # noqa
     except (AttributeError, KeyError) as error:
-        print('        ...not in model. Computing from scratch...')
+        lg.info('        ...not in model. Computing from scratch...')
         avg_cell_entropies = [bmsn.avg_cell_entropy(a) for a in model.schedule.agents]  # noqa
     ymax = max(avg_cell_entropies)
     if ymax <= 2.0:
         plt.ylim((-0.1, 2.1))
     else:
         plt.ylim((-0.1, ymax + 0.1))
-        print('WARN: maximum value over 2.0!', file=sys.stderr)
+        lg.warn('maximum value over 2.0!')
     plt.boxplot(generationalize(avg_cell_entropies, gen_count))
     plt.savefig('results/' + out_filename + '_avg_cell_entropy.png')
     plt.clf()
 
-    print('    cond_entropy...', file=sys.stderr)
+    lg.info('    cond_entropy...')
     try:
         cond_entropies = [get_data(a, bmsn.cond_entropy, 'cond_entropy')
                           for a in model.schedule.agents]
     except (AttributeError, KeyError) as error:
-        print('        ...not in model. Computing from scratch...')
+        lg.info('        ...not in model. Computing from scratch...')
         cond_entropies = [bmsn.cond_entropy(a) for a in model.schedule.agents]
     ymax = max(cond_entropies)
     if ymax <= 2.0:
         plt.ylim((-0.1, 2.1))
     else:
         plt.ylim((-0.1, ymax + 0.1))
-        print('WARN: maximum value over 2.0!', file=sys.stderr)
+        lg.warn('maximum value over 2.0!')
     plt.boxplot(generationalize(cond_entropies, gen_count))
     plt.savefig('results/' + out_filename + '_cond_entropy.png')
     plt.clf()
 
-    print('    mut_info...', file=sys.stderr)
+    lg.info('    mut_info...')
     mut_infos = [i - j for i, j in zip(avg_cell_entropies, cond_entropies)]
     ymax = max(mut_infos)
     if ymax <= 2.0:
         plt.ylim((-0.1, 2.1))
     else:
         plt.ylim((-0.1, ymax + 0.1))
-        print('WARN: maximum value over 2.0!', file=sys.stderr)
+        lg.warn('maximum value over 2.0!')
     plt.boxplot(generationalize(mut_infos, gen_count))
     plt.savefig('results/' + out_filename + '_mut_info.png')
     plt.clf()
 
     if bootstrapping:
-        print('    bootstrap_avg and bootstrap_p...', file=sys.stderr)
+        lg.info('    bootstrap_avg and bootstrap_p...')
         # TODO(RJR) update to use new function like get_data
         try:
             bootstrap_avgs = [a.data['bootstrap_avg'] for a in model.schedule.agents]  # noqa
             bootstrap_ps = [a.data['bootstrap_p'] for a in model.schedule.agents]  # noqa
         except (AttributeError, KeyError) as error:
-            print('        ...not in model. Computing from scratch...')
+            lg.info('        ...not in model. Computing from scratch...')
             bootstraps = [bmsn.bootstrap(a) for a in model.schedule.agents]
             bootstrap_avgs = [i[0] for i in bootstraps]
             bootstrap_ps = [i[1] for i in bootstraps]
@@ -203,7 +205,7 @@ def draw_boxplots(model, out_filename=OUT_FILENAME, bootstrapping=False):
             plt.ylim((-0.1, 2.1))
         else:
             plt.ylim((-0.1, ymax + 0.1))
-            print('WARN: maximum value over 2.0!', file=sys.stderr)
+            lg.warn('maximum value over 2.0!')
         plt.boxplot(generationalize(bootstrap_avgs, gen_count))
         plt.savefig('results/' + out_filename + '_bootstrap_avg.png')
         plt.clf()
@@ -212,7 +214,7 @@ def draw_boxplots(model, out_filename=OUT_FILENAME, bootstrapping=False):
             plt.ylim((-0.1, 2.1))
         else:
             plt.ylim((-0.1, ymax + 0.1))
-            print('WARN: maximum value over 2.0!', file=sys.stderr)
+            lg.warn('maximum value over 2.0!')
         plt.boxplot(generationalize(bootstrap_ps, gen_count))
         plt.savefig('results/' + out_filename + '_bootstrap_p.png')
         plt.clf()
@@ -222,7 +224,7 @@ if __name__ == '__main__':
     lg.basicConfig(filename='results/' + OUT_FILENAME + '.log',
                    filemode='w', level=lg.DEBUG,
                    format='%(asctime)s %(name)s %(levelname)s: %(message)s')
-    lg.getLogger().addHandler(lg.StreamHandler())
+    # lg.getLogger().addHandler(lg.StreamHandler())  # echoes log to stderr
     model = bmsn.MorphLearnModel(gen_size=GEN_SIZE,
                                  gen_count=GEN_COUNT,
                                  morph_filename=MORPH_FILENAME,
