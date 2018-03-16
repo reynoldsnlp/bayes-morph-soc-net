5920a9e51b3b48604c254a59a91c9a57872fa9eb
Robert Reynolds <rob@hikari.byu.edu>
2018-03-16 11:26:23 -0600

    more results

--- results/extract_input_table.sh
+++ results/extract_input_table.sh
@@ -7,6 +7,6 @@ do
 	do
 		table=$(echo $each | sed 's/\.log/_input_table.txt/g')
 		echo "generating ${table} ..."
-		paste /Users/robertreynolds/research/bayes-morph-soc-net/artificial-data/id_column_24.txt <(paste <(printf "typeFreq,$(grep lexeme_type_freq_list ${each} | cut -d [ -f 2 | sed 's/]//g' | sed 's/ //g')" | tr , \\n) "/Users/robertreynolds/research/bayes-morph-soc-net/artificial-data/data_${input}.txt" | cut -f 1,3-) > ${table}
+		paste ${HOME}/repos/bayes-morph-soc-net/artificial-data/id_column_24.txt <(paste <(printf "typeFreq,$(grep lexeme_type_freq_list ${each} | cut -d [ -f 2 | sed 's/]//g' | sed 's/ //g')" | tr , \\n) ${HOME}/repos/bayes-morph-soc-net/artificial-data/data_${input}.txt | cut -f 1,3-) > ${table}
 	done
 done
